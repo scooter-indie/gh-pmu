81fcdacdbf7ccaf6c789636aa01cfd69f2787cde
sbeals <scott.beals@gmail.com>
2025-12-03 14:01:03 -0700

    fix: Simplify release workflow - use default build

--- script/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build script for gh-extension-precompile
-# $1 is the output filename (e.g., dist/gh-pmu_v0.2.0_darwin-amd64)
-
-go build -ldflags="-X github.com/scooter-indie/gh-pmu/cmd.version=${GH_RELEASE_TAG}" -o "$1"
